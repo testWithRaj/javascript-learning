54602641b21b317dacc183bcce171a1febb39eba
rajAutomtaion <raj116629@gmail.com>
2026-07-29 22:25:15 +0530

    Initial commit:Javascript learning project

---
+++ Day01-Introduction/basics.js
@@ -0,0 +1,2 @@
+console.log("Hello, World!");
+console.log("My name is Raj Kumar and i am learning javascript");
---
+++ Day02-Variables/variables.js
@@ -0,0 +1,13 @@
+// variable is a container for storing data values 
+// in javascript, we use the var, let and const keywords to declare variables
+
+let name = "John"; // string variable
+let age = 30; // number variable
+const isStudent = true; // boolean variable
+
+
+console.log(name); 
+console.log(age);
+console.log(isStudent);
+
+console.log(typeof name); // string
