429a9e400c776b7015a5819155f2e38352d99acb
rajAutomtaion <raj116629@gmail.com>
2026-08-10 22:53:30 +0530

    Added template literals

--- Day03-DataTypes/datatypes.js
+++ Day03-DataTypes/datatypes.js
@@ -2,4 +2,4 @@ let name ="Raj";
 
 let age = 25; // number variable
 const isStudent = false; // boolean variable    
-console.log(name);  
+console.log(name);
---
+++ Day04-Literals/template_literals.js
@@ -0,0 +1,8 @@
+// Literals are fixed values that we insert directly into our code. They can be of different types, such as numbers, strings, booleans, arrays, and objects.
+
+let name ="Raj" ;
+let age =35;
+let company = "Experian Hyderabad"
+
+console.log(`My name is ${name} and I am ${age} years old and I work at ${company}.`); // Using template literals to insert variables into a string
+
