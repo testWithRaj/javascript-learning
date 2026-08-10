8ef2ef410a840e2f46ea70d2b1981abde3cf427b
rajAutomtaion <raj116629@gmail.com>
2026-08-10 23:03:44 +0530

    revising a bit

---
+++ Day04-Literals/revise.js
@@ -0,0 +1,7 @@
+
+let name ="Raj kumar";
+let age =31;
+let city ="Dhanbad";
+let occupation ="SDET";
+
+console.log(`My name is ${name} and my age is ${age}, I live in the ${city} city and my occupation is ${occupation}.`);
