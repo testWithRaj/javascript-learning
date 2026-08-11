812fbbde42c56376cdc162911b730fc9b9276e77
rajAutomtaion <raj116629@gmail.com>
2026-08-11 12:01:46 +0530

    Arithmetic opertor

---
+++ Day05-Operators/operator.js
@@ -0,0 +1,12 @@
+// in this section we will learn about operators in javascript
+//1 Arithmetic Operators
+let a =10;
+let b=20;
+
+console.log(a+b);
+console.log(b-a);
+
+console.log(a*b);
+console.log(b/a);
+
+
