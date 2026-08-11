ba9aa870be90e164b9e09c1100ea22c94b10f51e
rajAutomtaion <raj116629@gmail.com>
2026-08-11 12:12:45 +0530

    assignment op

--- Day05-Operators/operator.js
+++ Day05-Operators/operator.js
@@ -8,5 +8,13 @@ console.log(b-a);
 
 console.log(a*b);
 console.log(b/a);
+console.log(a%b);
 
+// Assignment Operators
+ 
+let age = 35 ; // = is assignment operator
 
+let c=30;
+c+=40; // c=c+40;
+
+// Comparison Operators > its used to compare two values
