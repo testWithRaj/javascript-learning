32f9ce7d67339967aa62b2903f0bb5a5ec1f823d
rajAutomtaion <raj116629@gmail.com>
2026-08-12 10:25:13 +0530

    nestedif

---
+++ Day06-ConditionalStatement/nestedif.js
@@ -0,0 +1,16 @@
+let age =9;
+
+hasLicense = true;
+
+if(age>=18)
+{
+    if(hasLicense)
+    {
+        console.log("You can drive")
+    }  
+    
+}
+
+else {
+        console.log("You are not eligible, get driving license");
+    }
