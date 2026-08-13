9d52ea8edbc497d96eaf7681fcca5947100cca5c
rajAutomtaion <raj116629@gmail.com>
2026-08-13 12:26:14 +0530

    learing objects

---
+++ Day09-Objects/basicobject.js
@@ -0,0 +1,19 @@
+//Object is used to store the values in key value pairs
+
+let person ={
+    name :"Raj",
+    age:31,
+    location :"hyderabad"
+}
+
+console.log(person.location);
+
+
+//Accesing objects, we can do using 2 methods
+
+// Changing the property 
+// objects are mutable
+
+person.age =36;
+
+console.log(person.age);
